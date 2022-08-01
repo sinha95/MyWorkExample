4a97f4a6756ff733961745f05eea2941f422c5e3
Mehuli1995 <mehulichaudhuri1995@gmail.com>
2022-08-01 19:35:35 +0530

    adding files

---
+++ src/test/java/Academy/validateTitle.java
@@ -0,0 +1,54 @@
+package Academy;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import org.testng.annotations.Test;
+
+import pageObjects.LandingPage;
+
+import resources.base;
+
+public class validateTitle extends base {
+	public WebDriver driver;
+	public static Logger log = LogManager.getLogger(base.class.getName());
+
+	@BeforeTest
+	public void initialize() throws IOException {
+
+		driver = initializeDriver();
+		log.info("Driver is initialized");
+
+		driver.get(prop.getProperty("url"));
+		log.info("Navigated to Home page");
+	}
+
+	@Test
+
+	public void validateAppTitle() throws IOException {
+
+		// one is inheritance
+		// creating object to that class and invoke methods of it
+		LandingPage l = new LandingPage(driver);
+		// compare the text from the browser with actual text.- Error..
+		Assert.assertEquals(l.getTitle().getText(), "FEATURED CO123URSES");
+		log.info("Successfully validated Text message");
+		log.info("Test completed");
+
+	}
+
+	@AfterTest
+	public void teardown() {
+
+		driver.close();
+
+	}
+
+}
---
+++ target/classes/base.java
@@ -0,0 +1,58 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class base {
+
+	public WebDriver driver;
+	public Properties prop;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\resources\\data.properties");
+
+		prop.load(fis);
+		String urlname= System.getProperty("Environment");
+		System.out.println(urlname);
+		
+		String browserName = prop.getProperty("browser");
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+		} else if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					System.getProperty("user.dir") + "\\src\\main\\java\\resources\\geckodriver.exe");
+
+			driver = new FirefoxDriver();
+
+		}
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+
+	}
+
+	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+
+	}
+
+}
